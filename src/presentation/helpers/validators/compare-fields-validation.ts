e063a9a43a845f5e862f49bd733c7659ea6f5eb5
milton-jr92 <milton.jr92@gmail.com>
2022-10-09 20:04:11 -0300

    fix: check field comparison to return the right param error

--- src/presentation/helpers/validators/compare-fields-validation.ts
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -11,8 +11,8 @@ export class CompareFieldsValidation implements Validation {
   }
 
   validate (input: any): Error {
-    if (!input[this.fieldName] !== input[this.fieldToCompareName]) {
-      return new InvalidParamError(this.fieldName)
+    if (input[this.fieldName] !== input[this.fieldToCompareName]) {
+      return new InvalidParamError(this.fieldToCompareName)
     }
   }
 }
